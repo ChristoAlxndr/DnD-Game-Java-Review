d831c95884a0ce20f943ae1ee3e84fedc109f5fc
Christopher Alexander <ChristoAlxndr@Gmail.com>
2016-09-29 10:35:55 -0400

    Initial Commit 09/29/2016 @ 10:35

---
+++ src/main/java/com/app/app.java
@@ -0,0 +1,54 @@
+package com.app;
+
+import java.util.Scanner;
+
+public class app {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		boolean userInputCorrect = false;
+		
+		
+		System.out.println("Welcome to Silly D 'n' D\n\n");
+				
+		do{
+		System.out.println("Would you like to play as a:\n" +
+				" 1) Human Wizard\n" +
+				" 2) Elf Royal\n" +
+				" 3) Twi'lek Jedi\n" +
+				" 4) Smurf Warrior\n" +
+				" 5) Turtle Ninja");
+
+		String userInput = sc.nextLine();
+		
+		
+		switch (userInput) {
+		case "1":
+			//Call to humanWizard methods here
+			break;
+			
+		case "2":
+			//Call to elfRoyal methods here
+			break;
+			
+		case "3":
+			//Call to twi'lekJedi methods here
+			break;
+		
+		case "4":
+			//Call to smurfWarrior methods here
+			break;
+		
+		case "5":
+			//Call to turtleNinja methods here
+			break;
+
+		default:
+			System.out.println("You're input was invalid.  Please enter valid input.\n\n");
+			break;
+			}
+		} while (userInputCorrect);
+	} 
+}
---
+++ src/main/java/com/interfaces/Wizzard.java
@@ -0,0 +1,12 @@
+package com.interfaces;
+
+public interface Wizzard extends ClassInterface {
+
+	// +0 
+	@Override
+	void changeHealth();
+
+	// + 200
+	@Override
+	void changeMagic();
+}
